c9f32d318ae1484d40e9338a144d6d04055f9bdc
Yago Henrique <yagohsp@unipam.edu.br>
2019-10-02 02:26:04 -0300

    Spot e User

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://yago:yago@cluster0-qv0kl.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.use(express.json());
 app.use(routes);
